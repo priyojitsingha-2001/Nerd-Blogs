426bbfb26c313f5b53f9686c99e662712ae8c9e6
Priyojit <priyojitsingha01@gmail.com>
2023-09-18 19:35:34 +0530

    [FTR] Connecting to mongodb

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 //express app
 const app = express();
+
+//Connect to mongodb
+const dbURI = "mongodb+srv://priyojit:6R1Y4eKOUtSdWAN1@cluster0.dljk7ur.mongodb.net/blogsdb?retryWrites=true&w=majority"
+mongoose.connect(dbURI)
+    .then((result) => {
+        console.log("Connected to db");
+        app.listen(3000);
+    }).catch(err => {
+        console.log(err);
+    })
 
 //register view engine
 app.set('view engine', 'ejs');
@@ -14,9 +25,9 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
     //temp blog data
     const blogs = [
-        // { title: "My blog1", snippet: "The Quick Brown Fox jumps over the lazy Dog, blog 1" },
-        // { title: "My blog2", snippet: "The Quick Brown Fox jumps over the lazy Dog, blog 2" },
-        // { title: "My blog3", snippet: "The Quick Brown Fox jumps over the lazy Dog, blog 3" }
+        { title: "My blog1", snippet: "The Quick Brown Fox jumps over the lazy Dog, blog 1" },
+        { title: "My blog2", snippet: "The Quick Brown Fox jumps over the lazy Dog, blog 2" },
+        { title: "My blog3", snippet: "The Quick Brown Fox jumps over the lazy Dog, blog 3" }
     ];
     res.render('index', { title: "Home", blogs });
 });
@@ -34,7 +45,6 @@ app.use((req, res) => {// this is a middleware
     res.status(404).render('404', { title: "Error 😵" });
 });
 
-//listenig to app
-app.listen(3000);
+
 
 
